acb5de8ed851c2459ee1e5bf59a858a11b2f623a
MeFisto94 <MeFisto94@users.noreply.github.com>
2019-01-07 23:13:50 +0100

    Crowd: More work towards working code

--- src/main/java/com/jme3/recast4j/Detour/Crowd/Crowd.java
+++ src/main/java/com/jme3/recast4j/Detour/Crowd/Crowd.java
@@ -1,7 +1,9 @@
 package com.jme3.recast4j.Detour.Crowd;
 
 import com.jme3.math.Vector3f;
+import com.jme3.recast4j.Detour.BetterDefaultQueryFilter;
 import com.jme3.recast4j.Detour.DetourUtils;
+import org.recast4j.detour.DefaultQueryFilter;
 import org.recast4j.detour.NavMesh;
 import org.recast4j.detour.QueryFilter;
 import org.recast4j.detour.crowd.CrowdAgent;
@@ -10,12 +12,17 @@ import org.recast4j.detour.crowd.debug.CrowdAgentDebugInfo;
 
 import java.util.function.IntFunction;
 
+/**
+ * TODO: Javadoc
+ * Important to know: Filters have to be done using the queryFilterFactory. If none is specified, the first filter
+ * (index 0) will be the {@link BetterDefaultQueryFilter} and all others will be filled with {@link DefaultQueryFilter}s
+ */
 public class Crowd extends org.recast4j.detour.crowd.Crowd {
     protected boolean debug;
     protected CrowdAgentDebugInfo debugInfo;
 
     public Crowd(int maxAgents, float maxAgentRadius, NavMesh nav) {
-        super(maxAgents, maxAgentRadius, nav);
+        this(maxAgents, maxAgentRadius, nav, i -> (i == 0 ? new BetterDefaultQueryFilter() : new DefaultQueryFilter()));
     }
 
     public Crowd(int maxAgents, float maxAgentRadius, NavMesh nav, IntFunction<QueryFilter> queryFilterFactory) {
@@ -53,5 +60,16 @@ public class Crowd extends org.recast4j.detour.crowd.Crowd {
         if (agent.idx != -1) {
             removeAgent(agent.idx);
         }
+    }
+
+    public boolean requestMoveToTarget(Vector3f to, long polyRef) {
+        if (polyRef == 0 || to == null) {
+            throw new IllegalArgumentException("Invalid Target (" + to + ", " + polyRef + ")");
+        }
+
+        // Unfortunately ag.setTarget is not an exposed API, maybe we'll write a dispatcher class if that bugs me too much
+        return getActiveAgents().stream()
+            .map(ca -> requestMoveTarget(ca.idx, polyRef, DetourUtils.toFloatArray(to)))
+            .allMatch(b -> b); // if all were successful, return true, else return false.
     }
 }
