f5b1af1de025abab6290ba93a42a676b8e1b2234
MeFisto94 <MeFisto94@users.noreply.github.com>
2019-10-29 15:29:52 +0100

    Extended the Scenario Interfaces

--- src/main/java/com/jme3/recast4j/test/AbstractUIAwareScenario.java
+++ src/main/java/com/jme3/recast4j/test/AbstractUIAwareScenario.java
@@ -1,14 +1,17 @@
 package com.jme3.recast4j.test;
 
 import com.jme3.recast4j.Detour.Crowd.Crowd;
+import org.recast4j.detour.NavMesh;
 
 public abstract class AbstractUIAwareScenario implements UIAwareScenario {
     protected String description;
     protected String name;
+    protected String[] maps;
 
-    public AbstractUIAwareScenario(String description, String name) {
+    public AbstractUIAwareScenario(String description, String name, String... maps) {
         this.description = description;
         this.name = name;
+        this.maps = maps;
     }
 
     @Override
@@ -22,5 +25,10 @@ public abstract class AbstractUIAwareScenario implements UIAwareScenario {
     }
 
     @Override
-    public abstract Crowd[] run();
+    public String[] getRecommendedMaps() {
+        return maps;
+    }
+
+    @Override
+    public abstract Crowd[] run(NavMesh navMesh);
 }
--- src/main/java/com/jme3/recast4j/test/UIAwareScenario.java
+++ src/main/java/com/jme3/recast4j/test/UIAwareScenario.java
@@ -6,4 +6,12 @@ package com.jme3.recast4j.test;
  */
 public interface UIAwareScenario extends Scenario {
     String getDescription();
+
+    /**
+     * This is a hint to the UI which maps this Scenario is interesting to be viewed on.<br />
+     * For every map known to the UI, there will be an entry in a drop-down field.<br />
+     * The UI will then load the map and run the Scenario against that.
+     * @return The "list" of recommended maps
+     */
+    String[] getRecommendedMaps();
 }
