7798b82debabd03776a435010ab3974be43276f1
MeFisto94 <MeFisto94@users.noreply.github.com>
2019-11-03 22:59:33 +0100

    Add getters and setters to CrowdAgent so that the position `npos` can be influenced.

--- src/main/java/com/jme3/recast4j/Detour/Crowd/CrowdAgent.java
+++ src/main/java/com/jme3/recast4j/Detour/Crowd/CrowdAgent.java
@@ -1,5 +1,8 @@
 package com.jme3.recast4j.Detour.Crowd;
 
+import com.jme3.math.Vector3f;
+import com.jme3.recast4j.Detour.DetourUtils;
+
 /**
  * This is the jme3-recast4j Wrapper around recast4j's CrowdAgent to provide additional behavior.<br />
  * Ghost: This Crowd Agent wont participate in moving but will block other agents like a "ghost".<br />
@@ -42,5 +45,30 @@ public class CrowdAgent extends org.recast4j.detour.crowd.CrowdAgent {
 
     public Crowd getCrowd() {
         return crowd;
+    }
+
+
+    /**
+     * Sets the position of this crowd agent as used in the simulation.<br />
+     * This <i>can</i> be used as feedback e.g. from the physics, but be warned that doing this in the mid of a
+     * pathfinding process <b>could</b> break some internal state, as the upstream docs would suggest re-adding the
+     * agent to the crowd.<br />
+     * A <b>good</b> use for this method however is to re-place the agent before
+     * {@link Crowd#requestMoveToTarget(org.recast4j.detour.crowd.CrowdAgent, Vector3f)} is called, for example when
+     * the visual representation of the agent has been moved externally.
+     * @param position The new position
+     * @see #getPosition()
+     */
+    public void setPosition(Vector3f position) {
+        DetourUtils.fillFloatArray(npos, position);
+    }
+
+    /**
+     * Gets the position of this crowd agent as used in the simulation.
+     * @return the crowd agent position
+     * @see #setPosition(Vector3f)
+     */
+    public Vector3f getPosition() {
+        return DetourUtils.createVector3f(npos);
     }
 }
