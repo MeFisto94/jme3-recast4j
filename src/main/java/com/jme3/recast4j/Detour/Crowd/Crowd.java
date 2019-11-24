MeFisto94/jme3-recast4j

commit 3d2d5edf183f03a747842424af67331e85bca16b

Breaking Change: Apply Functions will call with a velocity of null when an agent has to stop movement

src/main/java/com/jme3/recast4j/Detour/Crowd/Crowd.java
@@ -175,7 +175,7 @@ public class Crowd extends org.recast4j.detour.crowd.Crowd {
     }
 
     protected void applyMovement(org.recast4j.detour.crowd.CrowdAgent crowdAgent, Vector3f newPos, Vector3f velocity) {
-        float vel = velocity.length();
+        float vel = velocity == null ? 0f : velocity.length();
 
         log.debug("crowdAgent i={}, newPos={}, velocity={}[{}]", crowdAgent.idx, newPos, velocity, vel);
 
@@ -204,8 +204,12 @@ public class Crowd extends org.recast4j.detour.crowd.Crowd {
                     return; // Initial "return": Don't even bother with formation or other logic.
                 }
 
-                bcc.setWalkDirection(velocity);
-                bcc.setViewDirection(velocity.normalize());
+                if (velocity != null) {
+                    bcc.setWalkDirection(velocity);
+                    bcc.setViewDirection(velocity.normalize());
+                } else {
+                    bcc.setWalkDirection(Vector3f.ZERO);
+                }
 
                 /* Note: Unfortunately BetterCharacterControl does not expose getPhysicsLocation but it's tied to the
                  * SceneGraph Position
@@ -346,11 +350,14 @@ public class Crowd extends org.recast4j.detour.crowd.Crowd {
     @Override
     public boolean resetMoveTarget(int idx) {
         formationTargets[idx] = null;
+        CrowdAgent agnt = getAgent(idx);
+        applyMovement(agnt, agnt.getPosition(), null);
         return super.resetMoveTarget(idx);
     }
 
     public boolean resetMoveTarget(CrowdAgent agent) {
         assert agent.crowd == this;
+        applyMovement(agent, agent.getPosition(), null);
         return resetMoveTarget(agent.idx);
     }
 

src/main/java/com/jme3/recast4j/Detour/Crowd/ApplyFunction.java
@@ -3,6 +3,21 @@ package com.jme3.recast4j.Detour.Crowd;
 import com.jme3.math.Vector3f;
 import org.recast4j.detour.crowd.CrowdAgent;
 
+/**
+ * When an application needs more control over how the changes are propagated from this library, it can implement this
+ * interface and set the {@link MovementApplicationType} to <code>CUSTOM</code> using
+ * {@link Crowd#setApplicationType(MovementApplicationType)}. <br />
+ * Then register this function using {@link Crowd#setCustomApplyFunction(ApplyFunction)}.
+ * @author MeFisto94
+ */
 public interface ApplyFunction {
+    /**
+     * Apply the calculations from Recast to your custom representation (e.g. Entity Component System).<br />
+     * <b>Caution:</b> When an agent has reached his target, this method will be called but with a velocity value of
+     * <code>null</code>, to signalize that this agent shall stop moving entirely.
+     * @param crowdAgent The agent of the crowd (you can use the userdata value to identify it)
+     * @param newPos The position for this agent
+     * @param velocity The velocity for this agent (can be <code>null</code>!)
+     */
     void applyMovement(CrowdAgent crowdAgent, Vector3f newPos, Vector3f velocity);
 }

commit a19ec9b5d67de62dbfd39f4ddcca67524f594e13

Fix Regression introduced in 3d2d5edf183f03a747842424af67331e85bca16b

src/main/java/com/jme3/recast4j/Detour/Crowd/Crowd.java
@@ -231,6 +231,10 @@ public class Crowd extends org.recast4j.detour.crowd.Crowd {
                 throw new IllegalArgumentException("Unknown Application Type");
         }
 
+        if (velocity == null) {
+            return; // Bugfix: Don't get caught in an endless loop when this code already triggered a movement-stop
+        }
+
         // If we aren't currently forming.
         if (formationTargets[crowdAgent.idx] == null) {
             // @TODO: Move Proximity Detector into Crowd Agent to allow for custom detectors?